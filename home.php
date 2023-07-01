3ca91b3664a13c8b4ac98ea3c1a6e02845dad930
Sharad <sharadritu.58.69@gmail.com>
2023-07-01 21:21:06 +0545

    added login and register

--- home.php
+++ home.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['user_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
     <!-- custom css link  -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style1.css">
 
 </head>
 <body>
