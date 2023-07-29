d8d2e8cb7a308904bc86af13435a3890b36e09d3
Sharad <sharadritu.58.69@gmail.com>
2023-07-29 21:51:10 +0545

    changes to profile section

--- include/admin_header.php
+++ include/admin_header.php
@@ -38,7 +38,7 @@
             $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
          ?>
          <p><?= $fetch_profile['name']; ?></p>
-         <a href="../admin/update_profile.php" class="btn">update profile</a>
+         <!-- <a href="../admin/update_profile.php" class="btn">update profile</a> -->
          <div class="flex-btn">
             <a href="../admin/admin_register.php" class="option-btn">register</a>
             <a href="../admin/admin_login.php" class="option-btn">login</a>
